83fa6e538fdce32f42fead40b7ec3cdbefd5103f
Rgalitzdorfer <138243492+Rgalitzdorfer@users.noreply.github.com>
2024-07-04 21:52:03 -0400

    Fin

--- W7 Multiclass Logistic Regression Model.py
+++ W7 Multiclass Logistic Regression Model.py
@@ -143,5 +143,5 @@ correlation_matrix = numerical_data.corr()
 plt.figure(figsize=(12, 10))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
 plt.title('Correlation Matrix')
-plt.savefig(f'{output_directory}/Correlation_Matrix_LogReg.png')  # Save the plot
+plt.savefig(f'{output_directory}/Matrix_LogReg.png')  # Save the plot
 plt.show()
